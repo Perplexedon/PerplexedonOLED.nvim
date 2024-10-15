6b2012bd5e77760137ec99361f598c5efc7a3a0c
Perplexedon <perplexedon@gmail.com>
2024-10-16 09:32:26 +1000

    Create palette.lua

---
+++ lua/Perplexedon_OLED/palette.lua
@@ -0,0 +1,83 @@
+local colors = {
+    norm='#6c7e96', -- default foreground
+
+    bclr='#000000', -- background color hexadecimal
+    dark='#121212', -- dark color
+    drk2='#223f73', -- dark 2 color
+    whit='#d1e5ff', -- white color
+
+    culc='#272727', -- cursor line/column hexadecimal
+    comm='#3c4754', -- comment color
+
+    lit1='#127978', -- literal color 1
+    lit2='#148787', -- literal color 2
+    lit3='#127a57', -- literal color 3
+    ops1='#367cb0', -- operations color 1
+    ops2='#2b7694', -- operations color 2
+    ops3='#005faf', -- operations color 3
+    ops4='#395573', -- operations color 4
+    otag='#357b63', -- tag highlight color
+    lstr='#54667a', -- literal string highlight
+    incl='#005f87', -- include color
+    dlim='#4779b3', -- delimiter color
+    blod='#5f0000', -- bloody red
+
+    violet='#3D005D', -- violet
+
+    visu='#080808', -- visual highlight
+    high='#a5c1e6', -- highlight color
+    darkhigh='#7387a1', -- darker highlight color
+
+    var='#7387a1', -- function highlight
+    func='#7095b0', -- function highlight
+
+    dadd='#25533f', -- diff add
+    dchg='#00406d', -- diff change
+
+    clin='#131e30', -- cursor line
+    pmen='#6c7e96', -- pmenu color
+    csel='#005faf', -- search highlight color
+
+    cmpdef='#0c2430', -- cmp item
+
+    iden='#6289b3', -- identifier color
+
+    lbgn='#7095b0', -- light preprocessor color
+    dbng='#506a7d', -- dark preprocessor color
+    col1='#000408',
+    col2='#010912',
+    col3='#040f1c',
+    col4='#071526',
+    col5='#0c1c30',
+    col6='#10233a',
+    col7='#162b44',
+    col8='#1c334e',
+    col9='#233b58',
+    col10='#2b4462',
+    col11='#344d6c',
+    col12='#3f5876',
+    col13='#496280',
+    col14='#546c8a',
+    col15='#607794',
+    col16='#6d839e',
+    col17='#7c90a8',
+    col18='#8d9eb2',
+    col19='#95a7bc',
+    col20='#a4b3c6',
+    col21='#b7c2d0',
+    col22='#bfc8d0',
+    col23='#dbdfe4',
+    col24='#EEEEEE',
+
+    br1='#4064be',
+    br2='#4361b0',
+    br3='#455fa3',
+    br4='#465e9c',
+    br5='#475c94',
+    br6='#485a8c',
+    br7='#485885',
+
+    dnorm='#15181f'
+}
+
+return colors
