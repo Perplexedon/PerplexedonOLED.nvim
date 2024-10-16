54658bb6ae025e0f0eafea8b0496729049817c4e
Perplexedon <perplexedon@gmail.com>
2024-10-16 10:29:17 +1000

    Update palette.lua

--- lua/PerplexedonOLED/palette.lua
+++ lua/PerplexedonOLED/palette.lua
@@ -1,30 +1,27 @@
----@type Palette
+---@class Palette
 return {
-   bg = "#292A35", --
-   fg = "#F6F6F5",
-   selection = "#7C7F8A",
-   comment = "#70747f",
-   orange = "#FDC38E",
-   -- ANSI
-   black = "#1C1C1C", -- ANSI 0
-   red = "#DD6E6B",
-   green = "#87E58E",
-   yellow = "#E8EDA2",
-   purple = "#BAA0E8", -- used as ANSI 4 (blue)
-   pink = "#E48CC1",
-   cyan = "#A7DFEF",
-   white = "#F6F6F5", -- ANSI 7, 'selection' used for ANSI 8
-   -- indexes 9-15
-   bright_red = "#E1837F",
-   bright_green = "#97EDA2",
-   bright_yellow = "#F6F6B6",
-   bright_blue = "#D0B5F3",
-   bright_magenta = "#E7A1D7",
-   bright_cyan = "#BCF4F5",
-   bright_white = "#FFFFFF", -- index 15
-
+   bg = "#282A36",
+   fg = "#F8F8F2",
+   selection = "#44475A",
+   comment = "#6272A4",
+   red = "#FF5555",
+   orange = "#FFB86C",
+   yellow = "#F1FA8C",
+   green = "#50fa7b",
+   purple = "#BD93F9",
+   cyan = "#8BE9FD",
+   pink = "#FF79C6",
+   bright_red = "#FF6E6E",
+   bright_green = "#69FF94",
+   bright_yellow = "#FFFFA5",
+   bright_blue = "#D6ACFF",
+   bright_magenta = "#FF92DF",
+   bright_cyan = "#A4FFFF",
+   bright_white = "#FFFFFF",
    menu = "#21222C",
    visual = "#3E4452",
    gutter_fg = "#4B5263",
    nontext = "#3B4048",
+   white = "#ABB2BF",
+   black = "#191A21",
 }
