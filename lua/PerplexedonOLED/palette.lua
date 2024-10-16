69494d18a5f494add9aa340d8bd72d361c19ef4a
Perplexedon <perplexedon@gmail.com>
2024-10-16 10:21:48 +1000

    Update palette.lua

--- lua/PerplexedonOLED/palette.lua
+++ lua/PerplexedonOLED/palette.lua
@@ -1,83 +1,30 @@
-local colors = {
-    norm='#fff0bc', -- default foreground
+---@type Palette
+return {
+   bg = "#292A35", --
+   fg = "#F6F6F5",
+   selection = "#7C7F8A",
+   comment = "#70747f",
+   orange = "#FDC38E",
+   -- ANSI
+   black = "#1C1C1C", -- ANSI 0
+   red = "#DD6E6B",
+   green = "#87E58E",
+   yellow = "#E8EDA2",
+   purple = "#BAA0E8", -- used as ANSI 4 (blue)
+   pink = "#E48CC1",
+   cyan = "#A7DFEF",
+   white = "#F6F6F5", -- ANSI 7, 'selection' used for ANSI 8
+   -- indexes 9-15
+   bright_red = "#E1837F",
+   bright_green = "#97EDA2",
+   bright_yellow = "#F6F6B6",
+   bright_blue = "#D0B5F3",
+   bright_magenta = "#E7A1D7",
+   bright_cyan = "#BCF4F5",
+   bright_white = "#FFFFFF", -- index 15
 
-    bclr='#000000', -- background color hexadecimal
-    dark='#101010', -- dark color
-    drk2='#121212', -- dark 2 color
-    whit='#a4a4a4', -- white color
-
-    culc='#101010', -- cursor line/column hexadecimal
-    comm='#101010', -- comment color
-
-    lit1='#127978', -- literal color 1
-    lit2='#148787', -- literal color 2
-    lit3='#127a57', -- literal color 3
-    ops1='#367cb0', -- operations color 1
-    ops2='#2b7694', -- operations color 2
-    ops3='#005faf', -- operations color 3
-    ops4='#395573', -- operations color 4
-    otag='#357b63', -- tag highlight color
-    lstr='#54667a', -- literal string highlight
-    incl='#005f87', -- include color
-    dlim='#4779b3', -- delimiter color
-    blod='#5f0000', -- bloody red
-
-    violet='#3D005D', -- violet
-
-    visu='#080808', -- visual highlight
-    high='#a5c1e6', -- highlight color
-    darkhigh='#7387a1', -- darker highlight color
-
-    var='#7387a1', -- function highlight
-    func='#7095b0', -- function highlight
-
-    dadd='#25533f', -- diff add
-    dchg='#00406d', -- diff change
-
-    clin='#131e30', -- cursor line
-    pmen='#6c7e96', -- pmenu color
-    csel='#005faf', -- search highlight color
-
-    cmpdef='#0c2430', -- cmp item
-
-    iden='#6289b3', -- identifier color
-
-    lbgn='#7095b0', -- light preprocessor color
-    dbng='#506a7d', -- dark preprocessor color
-    col1='#000408',
-    col2='#010912',
-    col3='#040f1c',
-    col4='#071526',
-    col5='#0c1c30',
-    col6='#10233a',
-    col7='#162b44',
-    col8='#1c334e',
-    col9='#233b58',
-    col10='#2b4462',
-    col11='#344d6c',
-    col12='#3f5876',
-    col13='#496280',
-    col14='#546c8a',
-    col15='#607794',
-    col16='#6d839e',
-    col17='#7c90a8',
-    col18='#8d9eb2',
-    col19='#95a7bc',
-    col20='#a4b3c6',
-    col21='#b7c2d0',
-    col22='#bfc8d0',
-    col23='#dbdfe4',
-    col24='#EEEEEE',
-
-    br1='#4064be',
-    br2='#4361b0',
-    br3='#455fa3',
-    br4='#465e9c',
-    br5='#475c94',
-    br6='#485a8c',
-    br7='#485885',
-
-    dnorm='#15181f'
+   menu = "#21222C",
+   visual = "#3E4452",
+   gutter_fg = "#4B5263",
+   nontext = "#3B4048",
 }
-
-return colors
